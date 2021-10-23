5529e45ba7ea02a785b9c3cf3e073c71b814b308
Shahadat Hossen <shobujlingdu@gmail.com>
2021-10-23 17:57:55 +0600

    Dungeon Cleaner Game panel added

--- resources/views/dungeon-cleaner/tile-spawns/edit.blade.php
+++ resources/views/dungeon-cleaner/tile-spawns/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <form method="post" action="{{ route('dungeon-cleaner.tile-spawns.update', $dangerMeter) }}" autocomplete="off"
+                    <form method="post" action="{{ route('dungeon-cleaner.tile-spawns.update', $tileSpawn) }}" autocomplete="off"
                         class="form-horizontal" enctype="multipart/form-data">
                         @csrf
                         @method('put')
@@ -29,7 +29,7 @@
                                                             class="form-control{{ $errors->has('tile_spawn_name') ? ' is-invalid' : '' }}"
                                                             name="tile_spawn_name" id="input-tile_spawn_name" type="text"
                                                             placeholder="{{ __('Name') }}"
-                                                            value="{{ $dangerMeter->tile_spawn_name }}" required="true"
+                                                            value="{{ $tileSpawn->tile_spawn_name }}" required="true"
                                                             aria-required="true" />
                                                         @if ($errors->has('tile_spawn_name'))
                                                             <span id="tile_spawn_name-error" class="error text-danger"
@@ -51,7 +51,7 @@
                                                             class="form-control{{ $errors->has('tile_spawn_min') ? ' is-invalid' : '' }}"
                                                             name="tile_spawn_min" id="input-tile_spawn_min" type="number"
                                                             placeholder="{{ __('Min') }}"
-                                                            value="{{ $dangerMeter->tile_spawn_min }}" required="true"
+                                                            value="{{ $tileSpawn->tile_spawn_min }}" required="true"
                                                             aria-required="true" />
                                                         @if ($errors->has('tile_spawn_min'))
                                                             <span id="tile_spawn_min-error" class="error text-danger"
@@ -71,7 +71,7 @@
                                                             class="form-control{{ $errors->has('tile_spawn_max') ? ' is-invalid' : '' }}"
                                                             name="tile_spawn_max" id="input-tile_spawn_max" type="number"
                                                             placeholder="{{ __('Max') }}"
-                                                            value="{{ $dangerMeter->tile_spawn_max }}" required="true"
+                                                            value="{{ $tileSpawn->tile_spawn_max }}" required="true"
                                                             aria-required="true" />
                                                         @if ($errors->has('tile_spawn_max'))
                                                             <span id="tile_spawn_max-error" class="error text-danger"
